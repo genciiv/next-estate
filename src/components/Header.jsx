43c5ae304b8770063e73a8dacf18e0edd116d5ba
genciiv <vaqogenci@gmail.com>
2024-12-14 16:29:15 +0100

    Add the header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link href="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-red-600">GV-</span>
+            <span className="text-black">Estate</span>
+          </h1>
+        </Link>
+
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Kerko..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <button>
+            <FaSearch />
+          </button>
+        </form>
+
+        <ul className="flex gap-4">
+          <Link href="/">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              Kryefaqe
+            </li>
+          </Link>
+          <Link href="/about">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              Rreth Nesh
+            </li>
+          </Link>
+
+          <Link href="/sign-in">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              Logohu
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
